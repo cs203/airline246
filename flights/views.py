37cd2b3e753514f77cfac4ffbd0bcc934a653d47
cs203 <67407203+cs203@users.noreply.github.com>
2025-02-19 10:06:31 +0700

    passengers

--- flights/views.py
+++ flights/views.py
@@ -4,3 +4,9 @@ from .models import Flight
 # Create your views here.
 def index(request):
     return render(request, "flights/index.html", {"flights": Flight.objects.all})
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(id=flight_id)
+    passengers = flight.passengers.all()
+    return render(request, "flights/flight.html", {
+        "flight": flight, "passengers": passengers})
